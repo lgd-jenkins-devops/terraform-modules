88cb53ba57f5ec20b2a35e97e31dd951fb05a73b
Luis <luis@10.10.5.104>
2024-11-11 17:28:26 -0500

    set backend name

--- bucket/outputs.tf
+++ bucket/outputs.tf
@@ -1,3 +1,3 @@
 output "bucket_name" {
-  value = var.bucket-type == "backend" ?  : google_storage_bucket.backend_bucket["enabled"].name :  google_storage_bucket.static-site["enabled"].name
+  value = var.bucket-type == "backend" ?  google_storage_bucket.backend_bucket["enabled"].name :  google_storage_bucket.static-site["enabled"].name
 }
